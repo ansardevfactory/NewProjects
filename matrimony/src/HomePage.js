23f069b128d96ef009f9f6fc294e7b5d61afade8
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-06-14 16:33:58 +0530

    g

--- matrimony/src/HomePage.js
+++ matrimony/src/HomePage.js
@@ -1,66 +1,58 @@
 import React, { useState } from "react";
-import userpic from './images/userpic.png'
-import userpic2 from './images/user2.png'
+import userpic from "./images/userpic.png";
+import userpic2 from "./images/user2.png";
+import bg from "./images/header-inner1.jpg";
+import SingleDiv from "./SingleDiv";
+import logo from "./images/logo_icon.svg";
 
 const HomePage = () => {
-    
-    const [row1, setRow1]=useState([1,2,3,4])
-    const [row2, setRow2]=useState([1,2,3,4,5,6])
+  const [row1, setRow1] = useState([1, 2, 3, 4]);
+  const [row2, setRow2] = useState([1, 2, 3, 4, 5, 6]);
 
   return (
     <>
-      <div className="homepage_row1">
-        <img src={userpic}/>
-        <ul>
-            <li>MY HOME</li>
-            <li>SEARCH</li>
-            <li>MATCHES</li>
-            <li>MAILBOX</li>
-            <li>UPGRADE NOW</li>
-        </ul>
-        <img  src={userpic}/>
-      </div>
-      <div className="homepage_row2">
-        <button className="regular">Regular</button>
-        <button>PRIME</button>
-      </div>
-      <div className="homepage_row3">
-        <div className="homepage_row3_col1">
-          <div className="homepage_row3_col1_titlediv">
-            <div className="homepage_row3_col1_titlediv_accountdetails">
-              <img  src={userpic}/>
-              <label>Good Afternoon!</label>
-              <h4>Dony Joseph</h4>
-              <label>Profile Completeness 40%</label>
-              <label>Complete Your Profile {">"}</label>
+      <div
+        className="banner_row"
+        style={{
+          position: "relative",
+          background: `no-repeat url(${bg}) 0% /100%`,
+        }}
+      >
+        <img
+          src={require("./images/header-inner1.jpg")}
+          style={{ opacity: 0 }}
+        />
+        <div className="homepage_row1">
+          <div className="homepage_row1_row1">
+            <img src={logo} />
+            <ul>
+              <li>MY HOME</li>
+              <li>SEARCH</li>
+              <li>MATCHES</li>
+              <li>MAILBOX</li>
+              <li>UPGRADE NOW</li>
+            </ul>
+            <div className="populate_container">
+              <img className="icons profileicon" src={userpic} />
+              <div className="populate_list">
+                 <label>Logout</label>
+              </div>
             </div>
-            <label>
-              As a free member, you are unable to talk face to face with matches
-              you like.
-            </label>
-            <button>Upgrade to make video calls</button>
+          </div>
+          <div className="homepage_row1_row2">
+            <input />
+            <button>SEARCH</button>
           </div>
         </div>
-        <div className="homepage_row3_col2">
-            <label className="mainlabel">Your profile is pending verification!</label>
-            <div className="homepage_row3_col2_row2">
-                <label className="headlabel">Your Daily Recommendations for 6th Jun</label>
-                <div className="roundlist">
-                    {row1.map(()=>{
-                        return <div className="singleround"><img src={userpic}/> <label>Alphonsa Jose</label></div>
-                    })}
-                </div>
-            </div>
-            <div className="homepage_row3_col2_row3"> 
-                <div className="homepage_row3_col2_row3_row1">
-                <label className="headlabel">19 New Matches</label>
-                <div className="squarelist">
-                    {row2.map(()=>{
-                        return <div className="singlediv"><img src={userpic2}/> <label>Alphonsa Jose</label></div>
-                    })}
-                </div>
-            </div>
-            </div>
+      </div>
+
+      <div className="homepage_row3">
+        <div className="squarelist">
+          <SingleDiv />
+          <SingleDiv />
+          <SingleDiv />
+          <SingleDiv />
+          <SingleDiv />
         </div>
       </div>
     </>
--- matrimony/src/Navigation.js
+++ matrimony/src/Navigation.js
@@ -1,6 +1,7 @@
 import { HashRouter as BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./App";
 import HomePage from "./HomePage";
+import Profile from "./Profile";
 import Register from "./Register";
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path="/" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/homepage" element={<HomePage />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
     </BrowserRouter>
   );
